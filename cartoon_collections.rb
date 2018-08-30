90cadfd895fe8c0f979399cffb35106d4a11fcaa
Heather Miller <heather.miller114@gmail.com>
2018-08-30 13:46:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   new_calls = []
-  planeteer_calls.collect do 
+  planeteer_calls.collect { |call|
 end
 
 def long_planeteer_calls# code an argument here
